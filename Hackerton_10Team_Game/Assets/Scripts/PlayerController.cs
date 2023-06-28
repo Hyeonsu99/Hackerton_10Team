94116854c9293cf54c0921233c5ed69c73d4d64c
zachpaul7 <zachpaul707@gmail.com>
2023-06-29 00:39:33 +0900

    .

---
+++ Hackerton_10Team_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private PlayerHP_Controller playerHP_Controller;
+    private int life = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("")) //적 무기 태그 설정
+        {
+            life--;
+            if(life <= 0)
+            {
+                SceneManager.LoadScene("");  // GameOver Scene 불러오기
+            }
+        }
+    }
+}
