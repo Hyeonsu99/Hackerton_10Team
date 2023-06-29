e3c437264f571d6dca3f5196e5ab93e56095b0c6
Hyeonsu99 <johyeonsu08@gmail.com>
2023-06-29 16:29:37 +0900

    죽음 씬 완성 직전

---
+++ Hackerton_10Team_Game/Assets/GameScene/TestPlayermove.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class TestPlayermove : MonoBehaviour
+{
+
+    float speed = 2f;
+
+    float jumpspeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow)) 
+        { 
+            gameObject.transform.Translate(speed  * Time.deltaTime, 0, 0);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow)) 
+        {
+            gameObject.transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpspeed, ForceMode2D.Impulse);
+        }
+    }
+}
